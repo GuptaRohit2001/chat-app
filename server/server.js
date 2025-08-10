7b8e17b633d630af3fa8aa8dae840fbb833ca4fe
GuptaRohit2001 <imrohitgupta2001@gmail.com>
2025-08-10 19:37:58 +0530

    socket.io integrated

--- server/server.js
+++ server/server.js
@@ -1,13 +1,12 @@
-import express from 'express'
-import 'dotenv/config'
-import cors from 'cors'
-import http from 'http'
-import { connectDB } from './lib/db.js'
-import userRouter from './routes/userRoutes.js'
-import messageRouter from './routes/messageRoutes.js'
+import express from 'express';
+import 'dotenv/config';
+import cors from 'cors';
+import http from 'http';
+import { connectDB } from './lib/db.js';
+import userRouter from './routes/userRoutes.js';
+import messageRouter from './routes/messageRoutes.js';
 import { Server } from 'socket.io';
-import { Socket } from 'dgram'
-import { log } from 'console'
+
 //Create Express app and HTTP server
 const app=express()
 const server=http.createServer(app)
